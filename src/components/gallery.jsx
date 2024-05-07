0bd8f49a71c5cd9b03bb4a30d5f2875b20cd7dca
Larinda <larinda@excelhelp.ca>
2024-05-07 17:32:12 -0500

    Eleveth Commit

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -3,30 +3,19 @@ import React from "react";
 
 export const Gallery = (props) => {
   return (
-    <div id="portfolio" className="text-center">
-      <div className="container">
-        <div className="section-title">
-          <h2>Projects</h2>
-          <p>
-            
-          </p>
+    <div id="about">
+    <div className="container">
+      <div className="row">
+        <div className="col-xs-12 col-md-6">
+          {" "}
+          <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
         </div>
-        <div className="row">
-          <div className="portfolio-items">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div
-                    key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4"
-                  >
-                    <Image
-                      title={d.title}
-                      largeImage={d.largeImage}
-                      smallImage={d.smallImage}
-                    />
-                  </div>
-                ))
-              : "Loading..."}
+        <div className="col-xs-12 col-md-6">
+          <div className="about-text">
+            <h2>About Us</h2>
+            <p>{props.data ? props.data.paragraph : "loading..."}</p>
+
+            </div>
           </div>
         </div>
       </div>
