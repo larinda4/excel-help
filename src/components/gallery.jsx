190fb368b8e25e7d2519582fa20d5302ebcda4a9
Larinda <larinda@excelhelp.ca>
2024-05-07 22:00:47 -0500

    21st commit

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -8,7 +8,7 @@ export const Gallery = (props) => {
       <div className="row">
       <div className="col-xs-12 col-md-6">
           <div className="about-text">
-            <h2>Projects</h2>
+            <h2>Prior Projects</h2>
             <div className="col-lg-6 col-sm-6 col-xs-12">
             <p>{props.data ? props.data.paragraph : "loading..."}</p>
                 </div>
