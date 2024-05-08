fba4d2d25978b4f2c97adcb98aa11e15a18037dd
Larinda <larinda@excelhelp.ca>
2024-05-07 19:21:01 -0500

    Fourteenth Commit

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -8,9 +8,16 @@ export const Gallery = (props) => {
       <div className="row">
       <div className="col-xs-12 col-md-6">
           <div className="about-text">
-            <h2>About Us</h2>
-            <p>{props.data ? props.data.paragraph : "loading..."}</p>
-
+            <h2>Projects</h2>
+            <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
             </div>
           </div>
         <div className="col-xs-12 col-md-6">
